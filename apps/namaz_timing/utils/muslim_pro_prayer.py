08cf9f50da3fdb6733d005395dac7df7223b3e44
Shahzaib <mshahzaib1629@gmail.com>
2025-02-16 02:39:09 +0500

    processing months based on provided city

--- apps/namaz_timing/utils/muslim_pro_prayer.py
+++ apps/namaz_timing/utils/muslim_pro_prayer.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
@@ -16,9 +17,10 @@ def _find_project_root(current_path, marker_files=('.git', 'setup.py', 'requirem
         current_path = os.path.dirname(current_path)
     return None
 
-def _setup_driver():
+
+def _setup_driver() -> WebDriver:
     chrome_options = Options()
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
@@ -36,7 +38,7 @@ def _setup_driver():
     
     return driver
 
-def _handle_iframes(driver):
+def _handle_iframes(driver: WebDriver):
     try:
         iframes = driver.find_elements(By.TAG_NAME, "iframe")
         for iframe in iframes:
@@ -47,13 +49,24 @@ def _handle_iframes(driver):
     except Exception as e:
         print(f"Error while handling iframes: {e}")
 
-def _get_table_headers(driver, wait):
+def _set_city(driver: WebDriver, wait: WebDriverWait, city_name: str):
+    
+    city_input_ref = "//input[@id='searchPrayerArea']"
+    search_input = wait.until(EC.presence_of_element_located((By.XPATH, city_input_ref)))
+    search_input.clear()
+    search_input.send_keys(city_name)
+    
+    first_element_ref = "//div[@class='pac-container pac-logo hdpi']/div[@class='pac-item'][1]"
+    first_dropdown_item = wait.until(EC.presence_of_element_located((By.XPATH, first_element_ref)))
+    first_dropdown_item.click()
+
+def _get_table_headers(driver: WebDriver, wait: WebDriverWait):
     header_elements = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//table[@class='prayer-times']/tbody/tr[@class='p-2 text-center']/th")))
     headers = [header.text.strip() for header in header_elements]
     headers[0] = "Date"
     return headers
 
-def _scrape_table_data(driver, wait, headers):
+def _scrape_table_data(driver: WebDriver, wait: WebDriverWait, headers):
     row_elements = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//table[@class='prayer-times']/tbody/tr")))
     table_data = []
 
@@ -65,7 +78,7 @@ def _scrape_table_data(driver, wait, headers):
     
     return table_data
 
-def _close_overlay_if_present(driver, wait):
+def _close_overlay_if_present(driver: WebDriver, wait: WebDriverWait):
     try:
         overlay_close = wait.until(EC.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler")))
         overlay_close.click()
@@ -73,8 +86,35 @@ def _close_overlay_if_present(driver, wait):
     except:
         print("Overlay not found or already closed.")
 
+def _remove_overlay_ads(driver: WebDriver, wait: WebDriverWait):
+    try:
+        ad_path = "//*[starts-with(@id, 'gpt_unit_')]"
+        ads = driver.find_elements(By.XPATH, ad_path)
+        
+        for element in ads:
+            driver.execute_script("""
+            var element = arguments[0];
+            element.parentNode.removeChild(element);
+            """, element)
+            print(f"Removed element with id: {element.get_attribute('id')}")
+    except:
+        print("Ad not found")
+        
+def _remove_ad_frames(driver: WebDriver, wait: WebDriverWait):
+    try:
+        ad_path = "//*[starts-with(@id, 'google_ads_iframe_')]"
+        ads = driver.find_elements(By.XPATH, ad_path)
+        
+        for element in ads:
+            driver.execute_script("""
+            var element = arguments[0];
+            element.parentNode.removeChild(element);
+            """, element)
+            print(f"Removed element with id: {element.get_attribute('id')}")
+    except:
+        print("Ad not found")
 
-def _select_month(driver, month_index, wait):
+def _select_month(driver: WebDriver, month_index, wait: WebDriverWait):
     try:
 
         month_selection_button_path = "//div[@class='calender-div h-100 d-flex align-items-center p-3']/i"
@@ -84,12 +124,11 @@ def _select_month(driver, month_index, wait):
         month_selection_button = wait.until(EC.element_to_be_clickable((By.XPATH, month_selection_button_path)))
         month_selection_button.click()
         
-        print("Month Button Path: ", month_button_path)
         month_button = wait.until(EC.presence_of_element_located((By.XPATH, month_button_path)))
         month_button.click()
         wait.until(EC.staleness_of(month_button))
         
-
+        print("MONTH SELECTED for ", months_grid[month_index])        
         
         return True
     except Exception as e:
@@ -97,6 +136,9 @@ def _select_month(driver, month_index, wait):
         return False
 
 def scrap_prayer_timing_page():
+    
+    city_name = "Peshawar Pakistan"
+    
     url = "https://prayer-times.muslimpro.com/en/find?country_code=PK&country_name=Pakistan&city_name=Lahore&coordinates=31.5203696,74.35874729999999"
     driver = _setup_driver()
     wait = WebDriverWait(driver, 10)
@@ -104,14 +146,19 @@ def scrap_prayer_timing_page():
     driver.get(url)
     _handle_iframes(driver)
 
-    try:
+    try: 
+        _close_overlay_if_present(driver, wait)
+        
+        _set_city(driver, wait, city_name)
+        
         headers = _get_table_headers(driver, wait)
         month_index = 1
         
-        _close_overlay_if_present(driver, wait)
-        driver.execute_script("document.querySelector('.top-offer.fixed-top').remove()")
-        
         while month_index <= 12:
+            
+            _remove_overlay_ads(driver, wait)
+            _remove_ad_frames(driver, wait)
+            
             _select_month(driver, month_index, wait)
             
             table_data = _scrape_table_data(driver, wait, headers)
